a8f54a0c1415df9dc3598fa8c699e269ac0f0811
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2021-01-08 04:04:16 +0000

    attempt at fixes #13 might need rfc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
 import YstvHead from "../components/YstvHead";
 import VideoCarousel from "../components/VideoCarousel/VideoCarousel";
 import config from "../config.json";
-
+import { useState } from "react";
 import styles from "./index.module.css";
 import GenreBox from "../components/GenreBox/Genrebox";
 import Image from "next/image";
+import VideoPlayer from "../components/VideoPlayer/VideoPlayer";
 
 export default function Home({
   recentVideoPageState,
   popularVideoPageState,
   featuredVideoPageState,
   genreVideoPageState,
+  video,
 }) {
+  const [collapsed, setCollapsed] = useState(true);
+  const videoJSOptions = {
+    autoplay: false,
+    playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    controls: true,
+    fit: true,
+    fluid: true,
+    sources: video.files
+      .filter((e) => e.mode == "watch")
+      .map((e, i, t) => {
+        let sel = i == t.length - 1 ? true : false; // Sets to last item in list (assumed to be highest quality)
+        return {
+          src: `https://ystv.co.uk/videofile${e.uri.substring(6)}`,
+          type: e.mimeType,
+          label: `${e.height}p`,
+          selected: sel,
+        };
+      }),
+  };
+  var myplayer = VideoPlayer(videoJSOptions, 0);
   return (
     <>
       <YstvHead />
@@ -28,16 +50,34 @@ export default function Home({
             <div className={styles.bannerContents}>
               <h1>We are York Student Television.</h1>
               <h3>NaSTA Best Broadcaster and Best Tech 2019</h3>
-              <button className={styles.aboutButton}>About Us</button>
-              <button className={styles.highlightsButton}>Highlights</button>
-              {/* <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/3F076oLy_Lo"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              /> */}
+              <button
+                className={styles.aboutButton}
+                onClick={() => {
+                  window.location.href = "/about";
+                }}
+              >
+                About Us
+              </button>
+              <button
+                className={styles.highlightsButton}
+                onClick={() => setCollapsed(!collapsed)}
+              >
+                Highlights
+              </button>
+            </div>
+
+            <div
+              className={`${styles.collapsodiv} ${
+                collapsed ? styles.collapsodivcollapsed : ""
+              }`}
+            >
+              <button
+                className={styles.aboutButton}
+                onClick={() => setCollapsed(!collapsed)}
+              >
+                X
+              </button>
+              {myplayer}
             </div>
           </div>
         </div>
@@ -78,12 +118,19 @@ export async function getServerSideProps() {
       `${config.api.rest}/v1/public/videos/12/600`
     ).then((res) => res.json());
 
+    let video = await fetch(`${config.api.rest}/v1/public/video/3411`).then(
+      (res) => {
+        return res.json();
+      }
+    );
+
     return {
       props: {
         recentVideoPageState,
         popularVideoPageState,
         featuredVideoPageState,
         genreVideoPageState,
+        video,
       },
     };
   } catch {
