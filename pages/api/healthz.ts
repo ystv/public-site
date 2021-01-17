bc7e72d43c45e78d6f82307d9f06845c26c1db9e
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2021-01-17 05:03:34 +0000

    oops i guess typesafety doesn't matter at 5am

--- pages/api/healthz.ts
+++ pages/api/healthz.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default (req, res) => {
   res.status(200).json({ status: "ok!" });
 };
