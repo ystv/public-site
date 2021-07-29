40f6f5fc46bfd11b66b05d2c72085e3902ab814c
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2021-07-29 16:10:23 +0100

    about to try adding fader

--- pages/watch/index.tsx
+++ pages/watch/index.tsx
@@ -3,35 +3,39 @@ import VideoCarousel from "../../components/VideoCarousel/VideoCarousel";
 
 export default function Watch({ recentVideoPageState, oldVideoPageState }) {
   return (
-    <div className="mediumThin center">
+    <>
       <YstvHead />
-      <h1>Watch</h1>
-      <VideoCarousel title="Popular" videos={recentVideoPageState} detail />
+      <div className="thin center">
+        <h1>Watch</h1>
+      </div>
+      <div className="mediumThin center">
+        <VideoCarousel title="Popular" videos={recentVideoPageState} detail />
 
-      <VideoCarousel title="Featured" videos={oldVideoPageState} detail />
+        <VideoCarousel title="Featured" videos={oldVideoPageState} detail />
 
-      <VideoCarousel title="Recent" videos={recentVideoPageState} detail />
+        <VideoCarousel title="Recent" videos={recentVideoPageState} detail />
 
-      <VideoCarousel
-        title="Entertainment"
-        videos={recentVideoPageState}
-        detail
-      />
+        <VideoCarousel
+          title="Entertainment"
+          videos={recentVideoPageState}
+          detail
+        />
 
-      <VideoCarousel title="Factual" videos={recentVideoPageState} detail />
+        <VideoCarousel title="Factual" videos={recentVideoPageState} detail />
 
-      <VideoCarousel title="Scripted" videos={recentVideoPageState} detail />
+        <VideoCarousel title="Scripted" videos={recentVideoPageState} detail />
 
-      <VideoCarousel title="Sport" videos={recentVideoPageState} detail />
+        <VideoCarousel title="Sport" videos={recentVideoPageState} detail />
 
-      <VideoCarousel title="Archives" videos={recentVideoPageState} detail />
+        <VideoCarousel title="Archives" videos={recentVideoPageState} detail />
 
-      <VideoCarousel
-        title="Something Random"
-        videos={recentVideoPageState}
-        detail
-      />
-    </div>
+        <VideoCarousel
+          title="Something Random"
+          videos={recentVideoPageState}
+          detail
+        />
+      </div>
+    </>
   );
 }
 
