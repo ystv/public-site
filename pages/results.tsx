8a6d641e7db41b9b640b178524e19cb8ee8775ef
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2021-08-05 20:58:39 +0100

    Basic search

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,21 +1,38 @@
 import YstvHead from "../components/YstvHead";
+import VideoCell from "../components/VideoCell/VideoCell";
 
 export default function Results(props) {
   return (
     <main>
       <YstvHead />
       <div className="center thin">
-        <h1>These are results for &quot;{props.search}&quot; &rarr;</h1>
-        {props.res.map((e, i) => (
-          <h2 key={i}>{e}</h2>
-        ))}
+        <h1>Search results for &quot;{props.search}&quot;</h1>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          {props.res.videos ? (
+            props.res.videos.map((e, i) => <VideoCell video={e} key={i} />)
+          ) : (
+            <p>No results found</p>
+          )}
+        </div>
       </div>
     </main>
   );
 }
 
 export async function getServerSideProps(context) {
-  let res = ["ManMan", "Hunted 2", "Schubert^2"];
   let search = context.query.search;
+  let res = await fetch(`${process.env.REST_API}/v1/public/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: search }),
+  }).then((res) => res.json());
   return { props: { res, search } };
 }
