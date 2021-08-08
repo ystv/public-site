7e67879867c14bb1e5b78d336dda9bc577363d95
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2021-08-08 16:40:11 +0100

    navbar works with env vars too

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -82,7 +82,7 @@ const MenuList = ({ setMenuOpen }) => (
         link="/hires"
         text="Hires"
       />
-      <a href="https://my.ystv.co.uk">Login</a>
+      <a href={process.env.INTERNAL_SITE}>Login</a>
     </ul>
   </>
 );
