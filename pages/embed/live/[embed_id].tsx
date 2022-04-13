da25ac1b4912cf85f694b5e980366b959e3cfb5c
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2022-04-13 20:41:19 +0100

    live embed v1

--- pages/embed/live/[embed_id].tsx
+++ pages/embed/live/[embed_id].tsx
@@ -1,8 +1,51 @@
 import YstvHead from "../../../components/YstvHead";
-import Index from "../../../components/VideoPlayer";
+import VideoPlayer from "../../../components/VideoPlayer";
 
 import styles from "../embed.module.css";
+import { GetServerSideProps } from "next";
+import { channel } from "../../watch/live/[liveURLName]";
 
-export default function Embed({ video, time }) {
-  return <div className={styles.bg}>Embed for live video, coming soon</div>;
+export default function Embed({ channel }) {
+  const videoJSOptions = {
+    autoplay: false,
+    playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    height: 500,
+    controls: true,
+    liveui: true,
+    fill: true,
+    sources: [
+      {
+        src: channel.outputURL,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.bg}>
+      <YstvHead title={`Watch Live - ${channel.name}`} />
+      <VideoPlayer options={videoJSOptions} time={0} />
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  function redirect() {
+    context.res.statusCode = 302;
+    context.res.setHeader("Location", `/404`);
+  }
+
+  const channel: channel = await fetch(
+    `${process.env.REST_API}/v1/public/playout/channel/${context.query.embed_id}`
+  ).then((res) => {
+    if (!res.ok) {
+      redirect();
+    } else {
+      return res.json();
+    }
+  });
+
+  if (channel == undefined) redirect();
+
+  return { props: { channel } };
+};
