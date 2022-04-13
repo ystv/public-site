a482579e93810150de845bf44e4818c7e788302c
thekingofjibs <26507833+thekingofjibs@users.noreply.github.com>
2022-04-13 16:38:26 +0100

    fixed player on mobile

--- components/LiveFeaturedPlayerBanner/index.tsx
+++ components/LiveFeaturedPlayerBanner/index.tsx
@@ -1,45 +1,24 @@
 import styles from "./index.module.css";
-import Index from "../VideoPlayer";
 import { channel } from "../../pages/watch/live/[liveURLName]";
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import VideoPlayer from "../VideoPlayer";
 
 const LiveModal = ({ channel }: { channel: channel }) => {
-  const playerRef = useRef(null);
-
   const videoJSOptions = {
-    autoplay: true,
-    muted: true,
+    autoplay: false,
+    playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    height: 500,
     controls: true,
+    liveui: true,
     fluid: true,
+    poster: channel.thumbnail,
     sources: [
       {
         src: channel.outputURL,
+        type: "application/x-mpegURL",
       },
     ],
   };
-
-  const handlePlayerReady = (player) => {
-    playerRef.current = player;
-
-    // you can handle player events here
-    player.on("waiting", () => {
-      console.log("player is waiting");
-    });
-
-    player.on("dispose", () => {
-      console.log("player will dispose");
-    });
-  };
-
-  // const changePlayerOptions = () => {
-  //   // you can update the player through the Video.js player instance
-  //   if (!playerRef.current) {
-  //     return;
-  //   }
-  //   // [update player through instance's api]
-  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
-  //   playerRef.current.autoplay(false);
-  // };
 
   return (
     <div className={styles.container}>
@@ -51,22 +30,26 @@ const LiveModal = ({ channel }: { channel: channel }) => {
         <div
           style={{
             flex: 2,
+            width: "100%",
           }}
         >
-          <VideoJS options={videoJSOptions} onReady={handlePlayerReady} />
+          <VideoPlayer options={videoJSOptions} time={0} />
           <br />
         </div>
         <div style={{ flex: 1 }}>
-          <div>Channel 1</div>
-          <h2 style={{ margin: 0 }}>Title of Stream</h2>
-          <small>10-11pm</small>
-          <h5>Live from Central Hall</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ea
-            explicabo, fugiat hic ipsa itaque laborum mollitia porro quasi quia
-            quod rem repellendus repudiandae sequi voluptatum. Ad aliquid quos
-            repellendus?
-          </p>
+          {/*<div>Channel 1</div>*/}
+          <h2 style={{ margin: 0 }}>{channel.name}</h2>
+          <small>
+            {new Date(channel.scheduledStart).toLocaleTimeString("en-GB", {
+              timeStyle: "short",
+            })}
+            {" - "}
+            {new Date(channel.scheduledEnd).toLocaleTimeString("en-GB", {
+              timeStyle: "short",
+            })}
+          </small>
+          <h5>Live from {channel.location}</h5>
+          <p>{channel.description}</p>
         </div>
       </div>
     </div>
@@ -74,46 +57,3 @@ const LiveModal = ({ channel }: { channel: channel }) => {
 };
 
 export default LiveModal;
-
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
-
-const VideoJS = (props) => {
-  const videoRef = useRef(null);
-  const playerRef = useRef(null);
-  const { options, onReady } = props;
-
-  useEffect(() => {
-    // make sure Video.js player is only initialized once
-    if (!playerRef.current) {
-      const videoElement = videoRef.current;
-      if (!videoElement) return;
-
-      const player = (playerRef.current = videojs(videoElement, options, () => {
-        console.log("player is ready");
-        onReady && onReady(player);
-      }));
-    } else {
-      // you can update player here [update player through props]
-      // const player = playerRef.current;
-      // player.autoplay(options.autoplay);
-      // player.src(options.sources);
-    }
-  }, [options]);
-
-  // Dispose the Video.js player when the functional component unmounts
-  useEffect(() => {
-    return () => {
-      if (playerRef.current) {
-        playerRef.current.dispose();
-        playerRef.current = null;
-      }
-    };
-  }, []);
-
-  return (
-    <div data-vjs-player>
-      <video ref={videoRef} className="video-js vjs-big-play-centered" />
-    </div>
-  );
-};
