1038a5e82e73ccdbb8a3985163ba171ac511c291
Marks Polakovs <github@markspolakovs.me>
2023-09-06 15:35:51 +0100

    Replace map image with interactive Mapbox map (#64)

--- pages/find-us.tsx
+++ pages/find-us.tsx
@@ -1,12 +1,13 @@
 import YstvHead from "../components/YstvHead";
-import Image from "next/image"
-import MapImage from "../public/site-images/ystv_map.png"
+import Image from "next/image";
+import MapImage from "../public/site-images/ystv_map.png";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/src/css/mapbox-gl.css";
+import MapPin from "../public/site-images/Map-Pin-YSTV-Web.svg";
 
-export default function FindUs() {
-  return (
-    <div className="center thin">
-      <YstvHead />
-      <h1>Find Us</h1>
+function YSTVMap() {
+  if (!process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN) {
+    return (
       <Image
         src={MapImage}
         alt="Map showing YSTV's location on Campus West"
@@ -14,8 +15,36 @@ export default function FindUs() {
         sizes="100vw"
         style={{
           width: "100%",
-          height: "auto"
-        }} />
+          height: "auto",
+        }}
+      />
+    );
+  }
+  return (
+    <Map
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+      initialViewState={{
+        longitude: -1.05553,
+        latitude: 53.94636,
+        zoom: 14
+      }}
+      style={{width: "100%", height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      
+      >
+        <Marker longitude={-1.05553} latitude={53.94636} anchor="bottom">
+          <Image src={MapPin} alt="A map pin, with the YSTV Logo in the centre." width={64} height={64} />
+        </Marker>
+      </Map>
+  )
+}
+
+export default function FindUs() {
+  return (
+    <div className="center thin">
+      <YstvHead />
+      <h1>Find Us</h1>
+      <YSTVMap />
       <p>
         YSTV’s studio is in James College; if you need any help finding us just
         ask at Campus South Information or email us. There’s a courtyard in the
