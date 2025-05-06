b0b494f85f30851b5c89d19dcfd540eadef0e6c3
Liam Burnand <liam.burnand@ystv.co.uk>
2025-05-06 17:44:36 +0100

    Adding auth and cdn to image getters

--- next.config.js
+++ next.config.js
@@ -1,21 +1,33 @@
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx$/,
+    extension: /\.mdx$/,
 });
 const MDXExport = withMDX({
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  trailingSlash: true,
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    trailingSlash: true,
 });
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/login/internal",
-        destination: "https://old.ystv.co.uk/login/internal",
-        permanent: false,
-      },
-    ];
-  },
-  output: "standalone",
-  ...MDXExport,
-};
+    async redirects() {
+        return [
+            {
+                source: "/login/internal",
+                destination: "https://old.ystv.co.uk/login/internal",
+                permanent: false,
+            },
+        ];
+    },
+    output: "standalone",
+    ...MDXExport,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.ystv.co.uk',
+            },
+            {
+                protocol: 'https',
+                hostname: 'auth.ystv.co.uk',
+            }
+        ],
+    },
+}
