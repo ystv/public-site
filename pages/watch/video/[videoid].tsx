675011487b5b6b4f619d887d37e757936491fbd8
Liam Burnand <liam.burnand@ystv.co.uk>
2025-05-07 17:48:33 +0100

    Changed the popover and a few other things

--- pages/watch/video/[videoid].tsx
+++ pages/watch/video/[videoid].tsx
@@ -1,49 +1,43 @@
+import * as RadixPopover from "@radix-ui/react-popover";
 import YstvHead from "../../../components/YstvHead";
 import VideoPlayer from "../../../components/VideoPlayer";
 import Breadcrumb from "../../../components/Breadcrumb";
-import { formatTime } from "../../../components/commonFunctions";
-import { useState } from "react";
-import Popover from "react-popover";
+import {formatTime} from "../../../components/commonFunctions";
+import {useState} from "react";
 
 import styles from "./videoid.module.css";
 import {IBreadcrumb, VideoItem} from "../../../types/api/Video";
 
-export default function WatchVideo({ video, time, breadcrumb }) {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  const [copyButtonText, setCopyButtonText] = useState("Copy");
+export default function WatchVideo({video, time, breadcrumb}) {
+    const [copyButtonText, setCopyButtonText] = useState("Copy");
 
-  try {
-    if (!video) {
-      console.warn("No video data on video #", video.id);
-      return VideoErrorSnippet;
-    }
+    try {
+        if (!video || !video.files) {
+            console.warn("Video data or files missing.");
+            return VideoErrorSnippet;
+        }
 
-    if (!video.files) {
-      console.warn("No files on video #", video.id);
-      return VideoErrorSnippet;
-    }
-
-    const videoJSOptions = {
-      autoplay: false,
-      playbackRates: [0.5, 1, 1.25, 1.5, 2],
-      controls: true,
-      fill: true,
-      fluid: true,
-      sources: video.files
-        .filter((e) => e.mode == "watch")
-        .map((e, i, t) => {
-          let sel = i == t.length - 1 ? true : false; // Sets to last item in list (assumed to be highest quality)
-          return {
-            src:
-              e.uri.split("/")[0] == "legacy" // NEEDS to be replaced when API/storage migration is done, this is gross
-                ? `https://ystv.co.uk/videofile${e.uri.substring(6)}`
-                : `https://cdn.ystv.co.uk/${e.uri}`,
-            type: e.mimeType,
-            label: `${e.height}p`,
-            selected: sel,
-          };
-        }),
-    };
+        const videoJSOptions = {
+            autoplay: false,
+            playbackRates: [0.5, 1, 1.25, 1.5, 2],
+            controls: true,
+            fill: true,
+            fluid: true,
+            sources: video.files
+                .filter((e) => e.mode === "watch")
+                .map((e, i, t) => {
+                    const sel = i === t.length - 1;
+                    return {
+                        src:
+                            e.uri.split("/")[0] === "legacy"
+                                ? `https://ystv.co.uk/videofile${e.uri.substring(6)}`
+                                : `https://cdn.ystv.co.uk/${e.uri}`,
+                        type: e.mimeType,
+                        label: `${e.height}p`,
+                        selected: sel,
+                    };
+                }),
+        };
 
         const copyText = `<iframe
         src="https://ystv.co.uk/embed/${video.id}?height=360"
@@ -54,87 +48,86 @@ export default function WatchVideo({ video, time, breadcrumb }) {
         scrolling="no"
         ></iframe>`;
 
-    const popoverProps = {
-      isOpen: isPopoverOpen,
-      onOuterAction: () => setIsPopoverOpen(false),
-      body: (
-        <div className={styles.popover}>
-          <h1 key="a">Embed this video on your site:</h1>
-          <div
-            style={{ border: "solid", padding: "1rem", borderRadius: "1rem" }}
-            key="c"
-          >
-            {copyText}
-          </div>
-          <br />
-          <button
-            className={styles.embedButton}
-            key="b"
-            onClick={() => {
-              navigator.clipboard.writeText(copyText).then(
-                function () {
-                  console.log("Async: Copying to clipboard was successful!");
-                  setCopyButtonText("Copied");
-                },
-                function (err) {
-                  console.error("Async: Could not copy text: ", err);
-                }
-              );
-            }}
-          >
-            {copyButtonText}
-          </button>
-        </div>
-      ),
-    };
+        return (
+            <>
+                <YstvHead title={`Watch - ${video.name}`}/>
+                <div className="center thin">
+                    <Breadcrumb breadcrumb={breadcrumb}/>
+                    <h1>{video.name}</h1>
+                    <VideoPlayer options={videoJSOptions} time={time}/>
+                    <div className={styles.videoInfo}>
+                        <p
+                            dangerouslySetInnerHTML={{__html: video.description}}
+                            style={{flex: 1}}
+                        />
+                        <div className={styles.spacer}/>
+                        <div className={styles.rightInfo}>
+                            <h4>
+                                {video.views} View{video.views !== 1 ? "s" : null}
+                            </h4>
+                            <h5>Duration: {formatTime(video.duration)}</h5>
+                            <h5>
+                                Published{" "}
+                                {new Date(video.broadcastDate).toLocaleDateString("en-GB", {
+                                    day: "2-digit",
+                                    month: "2-digit",
+                                    year: "numeric",
+                                })}
+                            </h5>
 
-    return (
-      <>
-        <YstvHead title={`Watch - ${video.name}`} />
-        <div className="center thin">
-          <Breadcrumb breadcrumb={breadcrumb} />
-          <h1>{video.name}</h1>
-          <VideoPlayer options={videoJSOptions} time={time} />
-          <div className={styles.videoInfo}>
-            <p
-              dangerouslySetInnerHTML={{ __html: video.description }}
-              style={{ flex: 1 }}
-            />
-            <div className={styles.spacer} />
-            <div className={styles.rightInfo}>
-              <h4>
-                {video.views} View{video.views !== 1 ? "s" : null}
-              </h4>
-              <h5>Duration: {formatTime(video.duration)}</h5>
-              <h5>
-                Published{" "}
-                {new Date(video.broadcastDate).toLocaleString().split(",")[0]}
-              </h5>
-              {/*<button*/}
-              {/*  className={styles.embedButton}*/}
-              {/*  onClick={() => setIsPopoverOpen(true)}*/}
-              {/*>*/}
-              {/*  Embed*/}
-              {/*</button>*/}
-            </div>
-          </div>
-        </div>
-        {/*<Popover {...popoverProps}>*/}
-        {/*  <div />*/}
-        {/*  /!* Needs to be there coz reasons...? *!/*/}
-        {/*</Popover>*/}
-      </>
-    );
-  } catch (e) {
-    console.log(e);
-    return VideoErrorSnippet;
-  }
+                            {/* RADIX POPOVER INTEGRATION */}
+                            <RadixPopover.Root>
+                                <RadixPopover.Trigger asChild>
+                                    <button className={styles.embedButton}>Embed</button>
+                                </RadixPopover.Trigger>
+                                <RadixPopover.Portal>
+                                    <RadixPopover.Content
+                                        className={styles.popover}
+                                        sideOffset={8}
+                                    >
+                                        <h1>Embed this video on your site:</h1>
+                                        <div
+                                            style={{
+                                                border: "solid",
+                                                padding: "1rem",
+                                                borderRadius: "1rem",
+                                                marginTop: "0.5rem",
+                                            }}
+                                        >
+                                            {copyText}
+                                        </div>
+                                        <br/>
+                                        <button
+                                            className={styles.embedButton}
+                                            onClick={() => {
+                                                navigator.clipboard.writeText(copyText).then(
+                                                    () => setCopyButtonText("Copied"),
+                                                    (err) =>
+                                                        console.error("Async: Could not copy text: ", err)
+                                                );
+                                            }}
+                                        >
+                                            {copyButtonText}
+                                        </button>
+                                        <RadixPopover.Arrow className={styles.popoverArrow}/>
+                                    </RadixPopover.Content>
+                                </RadixPopover.Portal>
+                            </RadixPopover.Root>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    } catch (e) {
+        console.error(e);
+        return VideoErrorSnippet;
+    }
 }
 
 const VideoErrorSnippet = (
-  <div className="center thin">
-    <h1>Sorry! Video Could Not Be Loaded</h1>
-  </div>
+    <div className="center thin">
+        <h1>Sorry! Video Could Not Be Loaded</h1>
+    </div>
 );
 
 export async function getServerSideProps(context) {
